a96d6ea220804843bb510ad072cc72f55bb8735f
marioib1 <marioib1@informatica.iesvalledeljerteplasencia.es>
2023-09-18 18:59:53 +0200

    Cambio nombre

--- src/ActionBoton.java
+++ src/ActionBoton.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
  * De alguna manera tendrá que poder acceder a la ventana principal.
  * Se puede lograr pasando en el constructor la referencia a la ventana.
  * Recuerda que desde la ventana, se puede acceder a la variable de tipo ControlJuego
- * @author jesusredondogarcia
+ * @author Mario Iglesias Capa
  **
  */
 public class ActionBoton implements ActionListener{
